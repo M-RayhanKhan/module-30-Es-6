605d265e611fecdbd310bd27d7e380925f229f75
M-RayhanKhan <rayhansmartcode@gmail.com>
2024-09-27 18:26:32 +0600

    for loop

--- var-let-const.js
+++ var-let-const.js
@@ -1,4 +1,29 @@
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/M-RayhanKhan/module-30-Es-6.git
-// git push -u origin main
+
+
+const money = 30;
+// money = 50;
+// console.log(money);
+const numbers = [12, 32 ,55, 658, 89 , 47];
+numbers[0] = 10
+numbers.push(7, 11)
+// console.log(numbers);
+
+let myName = 'rayhan';
+myName = 'nasir'
+// console.log(myName);
+
+// object
+let friendName = {
+    name: 'Nasir',
+    age: 23,
+    designation: 'Web Development'
+} 
+friendName.name = 'Tapazzul'
+friendName.age = 25
+// console.log(friendName);
+
+let sum = 0
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(sum);
